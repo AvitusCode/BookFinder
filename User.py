8c12bfe07a56eb39c1f673a6de983f2129ccf58f
AvitusCode <rinogik97@gmail.com>
2024-01-06 21:28:18 +0300

    Make feature base recomendation system

--- User.py
+++ User.py
@@ -1,16 +1,14 @@
 
 
 class User(object):
-
-    years: str  # year-year
-    genres: str
-    countries: str
-    rating: str
-    duration: str  # diap in minutes
-
     def __init__(self, name, user_id):
         self.name = name
         self.user_id = user_id
+        self.years: str  # year-year
+        self.genres: str
+        self.countries: str
+        self.rating: str
+        self.duration: str  # diap in minutes
 
     def __str__(self):
         return f"{self.name}\nUserId: {self.user_id}\nPreferences:\nGenres:{self.genres}\nCountries{self.countries}\n" \
@@ -25,46 +23,3 @@ class User(object):
             "rating": self.rating,
             "duration": self.duration
         }
-
-    def in_years(self, year):
-        a, b = self.years.split("-")
-        if int(a) <= year <= int(b):
-            return 1.0
-        else:
-            return -1.0
-
-    def in_rating(self, rating):
-        a, b = self.rating.split("-")
-        if float(a) <= rating <= float(b):
-            return 1
-        else:
-            return -1
-
-    def in_duration(self, duration):
-        a, b = self.duration.split("-")
-        if int(a) <= duration <= int(b):
-            return 1.0
-        else:
-            return -1.0
-
-    def in_countries(self, countries):
-        data = self.countries.split(" ")
-        data = [word.lower() for word in data]
-        match = 0.0
-        for country in countries:
-            if country.lower() in data:
-                match += 1.0
-            else:
-                match -= 1.0
-        return match / len(data)
-
-    def in_genres(self, genres):
-        data = self.genres.split(" ")
-        data = [word.lower() for word in data]
-        match = 0.0
-        for genre in genres:
-            if genre.lower() in data:
-                match += 1.0
-            else:
-                match -= 1.0
-        return match / len(data)
